c8273ad472ea9d5d8f1856f110709c2aa8fbfad8
Mystjc <77810795+mystjc@users.noreply.github.com>
2024-08-01 22:38:54 -0400

    Add Sync Tool

---
+++ sync.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Simple shell script to sync theme updates from my dotfiles
+
+cp -ua ~/dotfiles/.config/fastfetch/. ~/Documents/GitHub/cobaltic-themes/themes/Fastfetch
+cp -ua ~/dotfiles/.config/joplin-desktop/. ~/Documents/GitHub/cobaltic-themes/themes/Joplin
+cp -ua ~/dotfiles/.config/kitty/. ~/Documents/GitHub/cobaltic-themes/themes/kitty
+cp -ua ~/dotfiles/.config/MangoHud/. ~/Documents/GitHub/cobaltic-themes/themes/MangoHud
+cp -ua ~/dotfiles/.config/rofi/. ~/Documents/GitHub/cobaltic-themes/themes/Rofi
+cp -ua ~/dotfiles/.config/starship/. ~/Documents/GitHub/cobaltic-themes/themes/Starship
+cp -ua ~/dotfiles/.config/wezterm/. ~/Documents/GitHub/cobaltic-themes/themes/WezTerm
