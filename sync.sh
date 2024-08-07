0a5a0f4091926915abd4fe3828c03ca1f78ad4d8
Mystjc <77810795+mystjc@users.noreply.github.com>
2024-08-07 14:49:52 -0400

    Update Theme

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Simple shell script to sync theme updates from my dotfiles
+# Simple shell script to sync theme updates
 
 cp -ua ~/dotfiles/.config/fastfetch/. ~/Documents/GitHub/cobaltic-themes/themes/Fastfetch
 cp -ua ~/dotfiles/.config/joplin-desktop/. ~/Documents/GitHub/cobaltic-themes/themes/Joplin
@@ -9,3 +9,8 @@ cp -ua ~/dotfiles/.config/MangoHud/. ~/Documents/GitHub/cobaltic-themes/themes/M
 cp -ua ~/dotfiles/.config/rofi/. ~/Documents/GitHub/cobaltic-themes/themes/Rofi
 cp -ua ~/dotfiles/.config/starship/. ~/Documents/GitHub/cobaltic-themes/themes/Starship
 cp -ua ~/dotfiles/.config/wezterm/. ~/Documents/GitHub/cobaltic-themes/themes/WezTerm
+
+cp -ua ~/.local/share/color-schemes/. ~/Documents/GitHub/cobaltic-themes/themes/KDE/KColorScheme
+cp -ua ~/.local/share/org.kde.syntax-highlighting/themes/. ~/Documents/GitHub/cobaltic-themes/themes/KDE/KSyntaxHighlighting
+cp -ua ~/.config/klassy/. ~/Documents/GitHub/cobaltic-themes/themes/KDE/Klassy
+cp -ua ~/.local/share/konsole/Cobaltic.colorscheme ~/Documents/GitHub/cobaltic-themes/themes/KDE/Konsole
