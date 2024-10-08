e96d4bca9ca69fdaa5c5f63e1f9a5786f44e66a3
Mystjc <77810795+mystjc@users.noreply.github.com>
2024-10-08 15:52:37 -0400

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -2,14 +2,17 @@
 
 # Simple shell script to sync theme updates
 
-cp -ua ~/dotfiles/.config/fastfetch/. ~/Documents/GitHub/cobaltic-themes/themes/Fastfetch
-cp -ua ~/dotfiles/.config/joplin-desktop/. ~/Documents/GitHub/cobaltic-themes/themes/Joplin
-cp -ua ~/dotfiles/.config/kitty/. ~/Documents/GitHub/cobaltic-themes/themes/kitty
-cp -ua ~/dotfiles/.config/MangoHud/. ~/Documents/GitHub/cobaltic-themes/themes/MangoHud
-cp -ua ~/dotfiles/.config/rofi/. ~/Documents/GitHub/cobaltic-themes/themes/Rofi
-cp -ua ~/dotfiles/.config/starship/. ~/Documents/GitHub/cobaltic-themes/themes/Starship
-cp -ua ~/dotfiles/.config/wezterm/. ~/Documents/GitHub/cobaltic-themes/themes/WezTerm
+cp -ua ~/dotfiles/.config/btop/. ~/Documents/GitHub/cobaltic/themes/Btop++
+cp -ua ~/dotfiles/.config/fastfetch/. ~/Documents/GitHub/cobaltic/themes/Fastfetch
+cp -ua ~/dotfiles/.config/joplin-desktop/. ~/Documents/GitHub/cobaltic/themes/Joplin
+cp -ua ~/dotfiles/.config/kitty/. ~/Documents/GitHub/cobaltic/themes/Kitty
+cp -ua ~/dotfiles/.config/lazygit/. ~/Documents/GitHub/cobaltic/themes/Lazygit
+cp -ua ~/dotfiles/.config/MangoHud/. ~/Documents/GitHub/cobaltic/themes/MangoHud
+cp -ua ~/dotfiles/.config/rofi/. ~/Documents/GitHub/cobaltic/themes/Rofi
+cp -ua ~/dotfiles/.config/starship/. ~/Documents/GitHub/cobaltic/themes/Starship
+cp -ua ~/dotfiles/.config/wezterm/. ~/Documents/GitHub/cobaltic/themes/WezTerm
+cp -ua ~/dotfiles/.config/yazi/. ~/Documents/GitHub/cobaltic/themes/Yazi
 
-cp -ua ~/.local/share/color-schemes/. ~/Documents/GitHub/cobaltic-themes/themes/KDE/KColorScheme
-cp -ua ~/.config/klassy/. ~/Documents/GitHub/cobaltic-themes/themes/KDE/Klassy
-cp -ua ~/.local/share/konsole/Cobaltic.colorscheme ~/Documents/GitHub/cobaltic-themes/themes/KDE/Konsole
+cp -ua ~/.local/share/color-schemes/. ~/Documents/GitHub/cobaltic/themes/KDE/KColorScheme
+cp -ua ~/.config/klassy/. ~/Documents/GitHub/cobaltic/themes/KDE/Klassy
+cp -ua ~/.local/share/konsole/Cobaltic.colorscheme ~/Documents/GitHub/cobaltic/themes/KDE/Konsole
